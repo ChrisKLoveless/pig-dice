96522925366afd369985cc50d4cd8baed4734ad2
Student <student@epicodus-7.hsd1.or.comcast.net>
2022-11-02 15:36:53 -0700

    add input for player 2 and rollDice function
    co-authored-by: Ryan Krueger <thegckid@gmail.com>

--- README.md
+++ README.md
@@ -9,46 +9,13 @@ Expected Output: let test = new Player("test", 0)
         test;
         Player {name: "test", score: 0}
 
-Describe: Player.prototype.addScore = function (num);
+Describe: Player.prototype.hold = function ();
 
-Test: "It will add current players score"
-Code: Player.prototype.addScore = function(numb) {
-	this.score += number;
+Test: "It should keep the players score for their turn and reset to previous turn"
+Code: Player.prototype.hold = function () {
+	
 }
-Expected Output: let test = new Player("test", 0);
-	test.addScore(5);
-	test;
-  Player {name: "test", score: 5}
-
-Describe: Player.prototype.resetScore();
-
-Test: "If the player rolls a 1, reset score to 0"
-Code: Player.prototype.resetScore = function(num) {
-	if (num === 1){
-		this.score = 0;
-	};
-};
-Expected Output: test{score: 0}
-
-Describe: Player.prototype.setScore = function (num);
-
-Test: "It will add current players score, or reset their total to 0 based on their number"
-Code: Player.prototype.setScore = function (num) {
-	if (num >= 2 && num <= 6) {
-		this.score += num;
-	} else if (num === 1) {
-		this.score = 0;
-	};
-};
-Expected Output: test{score:0}
-
-Describe: randomNum();
-
-Test: "It should give a random number from 1 to 6"
-Code: function randomNum(max) {
-		return Math.floor(Math.random() * max)
-	}
-Expected Output: randomNum(6);
+Expected Output:
 
 Describe: Player.prototype.diceRoll = function();
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // Business Logic for player
 var player;
+var player2;
+
 function Player(name) {
 	this.name = name;
 	this.score = 0;
@@ -11,29 +13,46 @@ Player.prototype.diceRoll = function () {
 	if (roll !== 1) {
 		this.score += roll;
 		this.turn += 1;
+		this.turnScore += roll;
 	} else if (roll === 1) {
-		this.score = 0;
+		this.score = this.score;
 		this.turn = 0;
 	};
 	return this.score;
 }
+
+Player.prototype.hold = function () {
+
+}
+
 // UI Logic
 function handleSubmit(event) {
 	event.preventDefault();
 	userName = document.getElementById("playerName").value;
+	userName2 = document.getElementById("playerName2").value;
 	player = new Player(userName);
+	player2 = new Player(userName2);
 	document.getElementById("diceRoll").classList.remove("invisible");
+	document.getElementById("hold").classList.remove("invisible");
 	document.querySelector("p#player1").innerText = player.name;
 	document.querySelector("p#player1Score").innerText = player.score;
 	document.querySelector("p#player1Turns").innerText = player.turn;
+	document.querySelector("p#player2").innerText = player2.name;
+	document.querySelector("p#player2Score").innerText = player2.score;
+	document.querySelector("p#player2Turns").innerText = player2.turn;
+	
 }
 
 function handleRoll(player) {
 	player.diceRoll();
+	player2.diceRoll();
 	console.log(player.score);
 	document.querySelector("p#player1Score").innerText = player.score;
 	document.querySelector("p#player1Turns").innerText = player.turn;
+	document.querySelector("p#player2Score").innerText = player2.score;
+	document.querySelector("p#player2Turns").innerText = player2.turn;
 }
+
 
 window.addEventListener("load", function () {
 	document.querySelector("form#newPlayer").addEventListener("submit", handleSubmit);
