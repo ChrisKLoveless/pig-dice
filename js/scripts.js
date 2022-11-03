281924fabbb04c2c433bbed219152b80534ee599
Student <student@epicodus-7.hsd1.or.comcast.net>
2022-11-03 10:22:35 -0700

    add hold prototype to our player object
    co-authored-by: Ryan Krueger <thegckid@gmail.com>
    co-authored-by: Anahi Ruiz <anahi.sulikey@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
-// Business Logic for player
+
 var player;
 var player2;
 
+//Pig Game Logic
+function PigGame() {
+	this.players = [];
+} 
+
+PigGame.prototype.addPlayer = function (player) {
+	this.players[player.name] = player;
+}
+// Player Logic
 function Player(name) {
 	this.name = name;
 	this.totalScore = 0;
-	this.turn = 0;
+	this.turn = 1;
 	this.tempScore = 0;
 }
 
@@ -18,45 +27,61 @@ Player.prototype.diceRoll = function () {
 		this.tempScore = 0;
 		this.turn += 1;
 	};
-	return this.tempScore;
+
 }
 
 Player.prototype.hold = function () {
-	this.totalScore = this.tempScore;
+	this.totalScore += this.tempScore;
 	this.tempScore = 0;
 };
 
 // UI Logic
+let game = new PigGame();
+
 function handleSubmit(event) {
 	event.preventDefault();
 	userName = document.getElementById("playerName").value;
 	userName2 = document.getElementById("playerName2").value;
 	player = new Player(userName);
 	player2 = new Player(userName2);
+	game.addPlayer(player);
+	game.addPlayer(player2);
 	document.getElementById("diceRoll").classList.remove("invisible");
 	document.getElementById("hold").classList.remove("invisible");
 	document.querySelector("p#player1").innerText = player.name;
-	document.querySelector("p#player1Score").innerText = player.score;
+	document.querySelector("p#player1Score").innerText = player.totalScore;
 	document.querySelector("p#player1Turns").innerText = player.turn;
 	document.querySelector("p#player2").innerText = player2.name;
-	document.querySelector("p#player2Score").innerText = player2.score;
+	document.querySelector("p#player2Score").innerText = player2.totalScore;
 	document.querySelector("p#player2Turns").innerText = player2.turn;
 }
 
 function handleRoll(player) {
 	player.diceRoll();
 	player2.diceRoll();
-	console.log(player.score);
-	document.querySelector("p#player1Score").innerText = player.score;
+	console.log(player.tempScore);
+	console.log(player.totalScore);
+	console.log(player2.tempScore);
+	console.log(player2.totalScore);
+	document.querySelector("p#player1Score").innerText = player.totalScore;
 	document.querySelector("p#player1Turns").innerText = player.turn;
-	document.querySelector("p#player2Score").innerText = player2.score;
+	document.querySelector("p#player2Score").innerText = player2.totalScore;
 	document.querySelector("p#player2Turns").innerText = player2.turn;
 }
 
+function handleHold (player) {
+	player.hold();
+	player2.hold();
+	document.querySelector("p#player1Score").innerText = player.totalScore;
+	document.querySelector("p#player2Score").innerText = player2.totalScore;
+}
 
 window.addEventListener("load", function () {
 	document.querySelector("form#newPlayer").addEventListener("submit", handleSubmit);
 	document.getElementById("diceRoll").addEventListener("click", function () {
 		handleRoll(player);
+		document.getElementById("hold").addEventListener('click', function () {
+			handleHold(player);
+		})
 	});
 });
